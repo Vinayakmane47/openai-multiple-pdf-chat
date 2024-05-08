599cd72195fc0a165c1e46b5133dd5b684f18942
Vinayak Mane <manevinayakv.47@gmail.com>
2024-05-09 01:05:07 +1000

    added process button

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://img.freepik.com/premium-photo/little-cute-robot-funny-virtual-assistant-bot-isolated-white-background-3d-icon-ai-generated_59529-4383.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -37,7 +37,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        <img src="https://www.shareicon.net/download/2015/08/06/81216_man_512x512.png">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
